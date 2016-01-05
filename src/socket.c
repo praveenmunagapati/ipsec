cda05150e272fe020221a2bd9cc504dc7507d9be
sungho kim <pugboy23@gmail.com>
2016-01-05 11:34:07 +0900

    Socket is modified
    * tcp socket is modified for multi core.

--- src/socket.c
+++ src/socket.c
@@ -1,6 +1,7 @@
 #include <net/ni.h>
 #include <util/map.h>
 #include <malloc.h>
+#include <thread.h>
 #define DONT_MAKE_WRAPPER
 #include <_malloc.h>
 #undef DONT_MAKE_WRAPPER
@@ -8,14 +9,19 @@
 #include "sp.h"
 #include "sa.h"
 #include "socket.h"
-#include "rwlock.h"
+#include "rwlock.h" 
+#define MSL	120 * 2
 
 typedef struct _Sockets {
-	RWLock* rwlock;
+	RWLock rwlock;
 	Map* socket_table;
 } Sockets;
 
 bool socket_ginit() {
+	int id = thread_id();
+	if(id != 0)
+		return false;
+
 	int count = ni_count();
 	for(int i = 0; i < count; i++) {
 		NetworkInterface* ni = ni_get(i);
@@ -25,18 +31,11 @@ bool socket_ginit() {
 			goto fail;
 		}
 		memset(sockets, 0, sizeof(Sockets));
-
-		sockets->rwlock = __malloc(sizeof(RWLock), ni->pool);
-		if(!sockets->rwlock) {
-			printf("Can't allocate sockets lock\n");
-			__free(sockets, ni->pool);
-			goto fail;
-		}
+		rwlock_init(&(sockets->rwlock));
 
 		sockets->socket_table = map_create(16, NULL, NULL, ni->pool);
 		if(!sockets->socket_table) {
 			printf("Can't create socket table\n");
-			__free(sockets->rwlock, ni->pool);
 			__free(sockets, ni->pool);
 			goto fail;
 		}
@@ -44,7 +43,6 @@ bool socket_ginit() {
 		if(!ni_config_put(ni, SOCKETS, sockets)) {
 			printf("Can't add sockets\n");
 			map_destroy(sockets->socket_table);
-			__free(sockets->rwlock, ni->pool);
 			__free(sockets, ni->pool);
 			goto fail;
 		}
@@ -63,9 +61,6 @@ fail:
 			map_destroy(sockets->socket_table);
 		}
 
-		if(sockets->rwlock)
-			__free(sockets->rwlock, ni->pool);
-
 		if(sockets)
 			__free(sockets, ni->pool);
 
@@ -74,15 +69,43 @@ fail:
 	return false;
 }
 
+void socket_gdestroy() {
+	int id = thread_id();
+	if(id != 0)
+		return;
+
+	int count = ni_count();
+	for(int i = 0; i < count; i++) {
+		NetworkInterface* ni = ni_get(i);
+		Sockets* sockets = ni_config_remove(ni, SOCKETS);
+		if(!sockets)
+			continue;
+
+		MapIterator iter;
+		map_iterator_init(&iter, sockets->socket_table);
+		while(map_iterator_has_next(&iter)) {
+			MapEntry* entry = map_iterator_next(&iter);
+			Socket* socket = entry->data;
+			socket_delete(ni, socket);
+		}
+		map_destroy(sockets->socket_table);
+
+		if(sockets)
+			__free(sockets, ni->pool);
+
+	}
+}
+
 Socket* socket_create(NetworkInterface* ni, SP* sp, SA* sa) {
 	Socket* socket = __malloc(sizeof(socket), ni->pool);
 	if(!socket)
 		return NULL;
 
+	rwlock_init(&socket->rwlock);
 	socket->sp = sp;
 	socket->sa = sa;
 	socket->fin = false;
-	socket->lifetime = 120 * 2; //second
+	socket->lifetime = MSL; //second
 
 	return socket;
 }
@@ -97,18 +120,18 @@ bool socket_add(NetworkInterface* ni, uint32_t src_ip, uint16_t src_port, uint32
 
 	uint64_t src_key = (uint64_t)src_ip << 32 | (uint64_t)src_port;
 	Map* _socket_table = map_get(socket_table, (void*)src_key);
-	rwlock_write_lock(sockets->rwlock);
+	rwlock_write_lock(&sockets->rwlock);
 	if(!_socket_table) {
 		_socket_table = map_create(8, NULL, NULL, ni->pool);
 		if(!_socket_table) {
-			rwlock_write_unlock(sockets->rwlock);
+			rwlock_write_unlock(&sockets->rwlock);
 
 			return false;
 		}
 
 		if(!map_put(socket_table, (void*)src_key, _socket_table)) {
 			map_destroy(_socket_table);
-			rwlock_write_unlock(sockets->rwlock);
+			rwlock_write_unlock(&sockets->rwlock);
 
 			return false;
 		}
@@ -120,11 +143,11 @@ bool socket_add(NetworkInterface* ni, uint32_t src_ip, uint16_t src_port, uint32
 			map_destroy(_socket_table);
 			map_remove(socket_table, (void*)src_key);
 		}
-		rwlock_write_unlock(sockets->rwlock);
+		rwlock_write_unlock(&sockets->rwlock);
 
 		return false;
 	}
-	rwlock_write_unlock(sockets->rwlock);
+	rwlock_write_unlock(&sockets->rwlock);
 
 	return true;
 }
@@ -140,10 +163,10 @@ Socket* socket_remove(NetworkInterface* ni, uint32_t src_ip, uint16_t src_port, 
 	}
 
 	uint64_t dest_key = (uint64_t)dest_ip << 32 | (uint64_t)dest_port;
-	rwlock_write_lock(sockets->rwlock);
+	rwlock_write_lock(&sockets->rwlock);
 	Socket* socket = map_remove(_socket_table, (void*)dest_key);
 	if(!socket) {
-		rwlock_write_unlock(sockets->rwlock);
+		rwlock_write_unlock(&sockets->rwlock);
 		return NULL;
 	}
 
@@ -152,7 +175,7 @@ Socket* socket_remove(NetworkInterface* ni, uint32_t src_ip, uint16_t src_port, 
 		map_remove(socket_table, (void*)src_key);
 	}
 
-	rwlock_write_unlock(sockets->rwlock);
+	rwlock_write_unlock(&sockets->rwlock);
 
 	return socket;
 }
@@ -161,12 +184,28 @@ Socket* socket_get(NetworkInterface* ni, uint32_t src_ip, uint16_t src_port, uin
 	Sockets* sockets = ni_config_get(ni, SOCKETS);
 	Map* socket_table = sockets->socket_table;
 
+	rwlock_read_lock(&sockets->rwlock);
 	uint64_t src_key = (uint64_t)src_ip << 32 | (uint64_t)src_port;
 	Map* _socket_table = map_get(socket_table, (void*)src_key);
 	if(!_socket_table) {
-			return NULL;
+		rwlock_read_unlock(&sockets->rwlock);
+		return NULL;
 	}
 
 	uint64_t dest_key = (uint64_t)dest_ip << 32 | (uint64_t)dest_port;
-	return map_get(_socket_table, (void*)dest_key);
+	Socket* socket =  map_get(_socket_table, (void*)dest_key);
+	socket_ref(socket);
+	rwlock_read_unlock(&sockets->rwlock);
+
+	return socket;
+}
+
+bool socket_ref(Socket* socket) {
+	rwlock_read_lock(&socket->rwlock);
+	return true;
+}
+
+bool socket_unref(Socket* socket) {
+	rwlock_read_unlock(&socket->rwlock);
+	return true;
 }
--- src/socket.h
+++ src/socket.h
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include "sp.h"
 #include "sa.h"
+#include "rwlock.h"
 
 #define	SOCKETS	"net.ipsec.sockets"
 
@@ -11,14 +12,18 @@ typedef struct _Socket{
 	SP* sp;
 	SA* sa;
 	bool fin;
+	RWLock rwlock;
 	uint32_t lifetime;
 } Socket;
 
 bool socket_ginit();
+void socket_gdestroy();
 Socket* socket_create(NetworkInterface* ni, SP* sp, SA* sa);
 void socket_delete(NetworkInterface* ni, Socket* socket);
 bool socket_add(NetworkInterface* ni, uint32_t src_ip, uint16_t src_port, uint32_t dest_ip, uint16_t dest_port, Socket* socket);
 Socket* socket_remove(NetworkInterface* ni, uint32_t src_ip, uint16_t src_port, uint32_t dest_ip, uint16_t dest_port);
 Socket* socket_get(NetworkInterface* ni, uint32_t src_ip, uint16_t src_port, uint32_t dest_ip, uint16_t dest_port);
+bool socket_ref(Socket* socket);
+bool socket_unref(Socket* socket);
 
 #endif /*__SOCKET_H__*/
