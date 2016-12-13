b5c7f9672c2e50a213c75de5510e0c6f6846b135
sungho kim <sungho@gurum.cc>
2016-12-13 17:12:40 +0900

    SAD Accessor has been fixed
    * Debugging Message has been added.
    * When It add sa or sp, It dumps informations.

--- tool/sad_accessor/src/main.c
+++ tool/sad_accessor/src/main.c
@@ -1,30 +1,70 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <linux/pfkeyv2.h>
+#include <stdlib.h>
+#include <malloc.h>
 
 #include "sadb.h"
 #include "debug.h"
+#include "sapd.h"
+
+#define BUFFER_SIZE 102400
+void* get_shared_memory(int vmid) {
+	//TODO
+	return sapd_create();
+}
+void dummy_init() {
+	extern void* __gmalloc_pool;
+	extern size_t init_memory_pool(size_t, void *, int);
+	void* buffer = malloc(BUFFER_SIZE);
+ 	printf("buffer:%p\n", buffer);
+	init_memory_pool(BUFFER_SIZE, (void*)buffer, 0);
+	__gmalloc_pool = buffer;
+}
 
 int main(int argc, char** argv) {
-	int vmid = 0;
+	//TODO fix here
+	dummy_init();
 
-	if(sadb_set(vmid)) {
-		printf("Can't get shared buffer\n");
+	printf("--------------------------------\n");
+	printf("*** PacketNgin SAPD Accessor ***\n");
+	printf("--------------------------------\n");
+	if(argc != 2) {
+		printf("Virtual Machine ID is null\n");
+		printf("./sad_accessor [vmid]\n");
 		return -1;
 	}
 
-	int fd = sadb_connect();
-	if(fd < 0) {
-		printf("Can't connect SAD\n");
-		return -2;
-	}
+	int vmid = strtol(argv[1], NULL, 10);
+	printf("PacketNgin Virtual Machine: %d\n", vmid);
 
-	sadb_dump(fd);
-	sadb_process(fd);
-	sadb_disconnect(fd);
+	while(1) {
+		//TODO check machine state
+		void* shared_memory = get_shared_memory(vmid);
+		if(!shared_memory)
+			continue;
+
+		printf("Virtual Machine Shared Memory Address: %p\n", shared_memory);
+		if(!sapd_check(shared_memory)) {
+			printf("Shared Memory has not setted\n");
+			return -1;
+		}
+		SAPD* sapd = shared_memory;
+		int fd = sadb_connect();
+		if(fd < 0) {
+			printf("Can't connect SAD\n");
+			return -2;
+		}
+		sadb_dump(fd);
+		sadb_x_spddump(fd);
+		while(1) {
+			// TODO select 
+			// TODO check machine state
+			if(sadb_process(fd, sapd))
+				break;
+		}
+		sadb_disconnect(fd);
+	}
 
 	return 0;
 }
--- tool/sad_accessor/src/sadb.c
+++ tool/sad_accessor/src/sadb.c
@@ -4,10 +4,13 @@
 #include <unistd.h>
 #include <netinet/in.h>
 
+#include <sa.h>
+
+#include "sapd.h"
 #include "sadb.h"
 #include "debug.h"
 
-static bool sadb_update_process(struct sadb_msg* recv_msg) {
+static bool sadb_update_process(SAPD* sapd, struct sadb_msg* recv_msg) {
 	int len = recv_msg->sadb_msg_len - (sizeof(struct sadb_msg) / 8);
 	struct sadb_ext* sadb_ext = (struct sadb_ext*)((uint8_t*)recv_msg + sizeof(struct sadb_msg));
 	struct sadb_sa* sadb_sa = NULL;
@@ -39,12 +42,14 @@ static bool sadb_update_process(struct sadb_msg* recv_msg) {
 			case SADB_EXT_SENSITIVITY:
 				break;
 			default:
-				DEBUG_PRINT("type: %d\n", sadb_ext->sadb_ext_type);
+				DEBUG_PRINT("Error: sadb_ext_type = %d\n", sadb_ext->sadb_ext_type);
 				break;
 		}
 
-		if(len < sadb_ext->sadb_ext_len)
+		if(len < sadb_ext->sadb_ext_len) {
+			DEBUG_PRINT("Error: len < sadb_ext->sadb_ext_len\n");
 			return false;
+		}
 
 		len -= sadb_ext->sadb_ext_len;
 		sadb_ext = (struct sadb_ext*)((uint8_t*)sadb_ext + sadb_ext->sadb_ext_len * 8);
@@ -54,7 +59,7 @@ static bool sadb_update_process(struct sadb_msg* recv_msg) {
 	return true;
 }
 
-static bool sadb_add_process(struct sadb_msg* recv_msg) {
+static bool sadb_add_process(SAPD* sapd, struct sadb_msg* recv_msg) {
 	int len = recv_msg->sadb_msg_len - (sizeof(struct sadb_msg) / 8);
 	struct sadb_ext* sadb_ext = (struct sadb_ext*)((uint8_t*)recv_msg + sizeof(struct sadb_msg));
 	struct sadb_sa* sadb_sa = NULL;
@@ -82,13 +87,14 @@ static bool sadb_add_process(struct sadb_msg* recv_msg) {
 			case SADB_EXT_SENSITIVITY:
 				break;
 			default:
-				//TODO error
-				DEBUG_PRINT("Error\n");
+				DEBUG_PRINT("Error: sadb_ext_type = %d\n", sadb_ext->sadb_ext_type);
 				break;
 		}
 
-		if(len < sadb_ext->sadb_ext_len)
+		if(len < sadb_ext->sadb_ext_len) {
+			DEBUG_PRINT("Error: len < sadb_ext->sadb_ext_len\n");
 			return false;
+		}
 		len -= sadb_ext->sadb_ext_len;
 		sadb_ext = (struct sadb_ext*)((uint8_t*)sadb_ext + sadb_ext->sadb_ext_len * 8);
 	}
@@ -97,7 +103,7 @@ static bool sadb_add_process(struct sadb_msg* recv_msg) {
 	return true;
 }
 
-static bool sadb_delete_process(struct sadb_msg* recv_msg) {
+static bool sadb_delete_process(SAPD* sapd, struct sadb_msg* recv_msg) {
 	int len = recv_msg->sadb_msg_len - (sizeof(struct sadb_msg) / 8);
 	struct sadb_ext* sadb_ext = (struct sadb_ext*)((uint8_t*)recv_msg + sizeof(struct sadb_msg));
 	struct sadb_sa* sadb_sa = NULL;
@@ -117,12 +123,14 @@ static bool sadb_delete_process(struct sadb_msg* recv_msg) {
 				sadb_address_destination = (struct sadb_address*)sadb_ext;
 				break;
 			default:
-				DEBUG_PRINT("Error\n");
+				DEBUG_PRINT("Error: sadb_ext_type = %d\n", sadb_ext->sadb_ext_type);
 				break;
 		}
 
-		if(len < sadb_ext->sadb_ext_len)
+		if(len < sadb_ext->sadb_ext_len) { 
+			DEBUG_PRINT("Error: len < sadb_ext->sadb_ext_len\n");
 			return false;
+		}
 		len -= sadb_ext->sadb_ext_len;
 		sadb_ext = (struct sadb_ext*)((uint8_t*)sadb_ext + sadb_ext->sadb_ext_len * 8);
 	}
@@ -131,70 +139,78 @@ static bool sadb_delete_process(struct sadb_msg* recv_msg) {
 	return true;
 }
 
-static bool sadb_get_process(struct sadb_msg* recv_msg) {
+static bool sadb_get_process(SAPD* sapd, struct sadb_msg* recv_msg) {
 	int len = recv_msg->sadb_msg_len - (sizeof(struct sadb_msg) / 8);
-	struct sadb_ext* sadb_ext = (struct sadb_ext*)((uint8_t*)recv_msg + sizeof(struct sadb_msg));
+	SA* sa = sa_alloc(len * 8);
+	sa->len = len;
+	memcpy(sa->data, (uint8_t*)recv_msg + sizeof(struct sadb_msg), len * 8);
+	struct sadb_ext* sadb_ext = (struct sadb_ext*)(sa->data);
 	while(len) {
 		switch(sadb_ext->sadb_ext_type) {
 			case SADB_EXT_RESERVED:
 				DEBUG_PRINT("reserved %d\n", sadb_ext->sadb_ext_type);
 				break;
 			case SADB_EXT_SA:
-				;
-				struct sadb_sa* sadb_sa = (struct sadb_sa*)sadb_ext;
-				DEBUG_PRINT("sa %d\n", sadb_sa->sadb_sa_exttype);
+				sa->sa = (struct sadb_sa*)sadb_ext;
 				break;
 			case SADB_EXT_LIFETIME_CURRENT:
+				sa->lifetime_current = (struct sadb_lifetime*)sadb_ext;
+				break;
 			case SADB_EXT_LIFETIME_HARD:
+				sa->lifetime_hard = (struct sadb_lifetime*)sadb_ext;
+				break;
 			case SADB_EXT_LIFETIME_SOFT:
-				;
-				struct sadb_lifetime* sadb_lifetime = (struct sadb_lifetime*)sadb_ext;
-				DEBUG_PRINT("lifetime %d\n", sadb_lifetime->sadb_lifetime_exttype);
+				sa->lifetime_soft = (struct sadb_lifetime*)sadb_ext;
 				break;
 			case SADB_EXT_ADDRESS_SRC:
+				sa->address_src = (struct sadb_address*)sadb_ext;
+				break;
 			case SADB_EXT_ADDRESS_DST:
+				sa->address_dst = (struct sadb_address*)sadb_ext;
+				break;
 			case SADB_EXT_ADDRESS_PROXY:
-				;
-				struct sadb_address* sadb_address = (struct sadb_address*)sadb_ext;
-				DEBUG_PRINT("address %d\n", sadb_address->sadb_address_exttype);
-				DEBUG_PRINT("%d\n", sadb_address->sadb_address_len);
-				struct sockaddr_in* sockaddr_in = (struct sockaddr_in*)((uint8_t*)sadb_address + sizeof(struct sadb_address));
-				DEBUG_PRINT("%x\n", sockaddr_in->sin_addr.s_addr);
-				DEBUG_PRINT("%d\n", sockaddr_in->sin_port);
-				 
+				sa->address_proxy = (struct sadb_address*)sadb_ext;
 				break;
 			case SADB_EXT_KEY_AUTH:
+				sa->key_auth = (struct sadb_key*)sadb_ext;
+				break;
 			case SADB_EXT_KEY_ENCRYPT:
-				;
-				struct sadb_key* sadb_key = (struct sadb_key*)sadb_ext;
-				DEBUG_PRINT("key %d\n", sadb_key->sadb_key_exttype);
+				sa->key_encrypt = (struct sadb_key*)sadb_ext;
 				break;
 			case SADB_EXT_IDENTITY_SRC:
+				sa->identity_src = (struct sadb_ident*)sadb_ext;
+				break;
 			case SADB_EXT_IDENTITY_DST:
-				;
-				struct sadb_ident* sadb_ident = (struct sadb_ident*)sadb_ext;
-				DEBUG_PRINT("ident %d\n", sadb_ident->sadb_ident_exttype);
+				sa->identity_dst = (struct sadb_ident*)sadb_ext;
 				break;
 			case SADB_EXT_SENSITIVITY:
-				;
-				struct sadb_sens* sadb_sens = (struct sadb_sens*)sadb_ext;
-				DEBUG_PRINT("sens %d\n", sadb_sens->sadb_sens_exttype);
+				sa->sensitivity = (struct sadb_sens*)sadb_ext;
+				break;
+			case SADB_X_EXT_SA2:
+				sa->x_sa2 = (struct sadb_x_sa2*)sadb_ext;
 				break;
 			default:
-				DEBUG_PRINT("Error\n");
+				DEBUG_PRINT("Error: sadb_ext_type = %d\n", sadb_ext->sadb_ext_type);
 				break;
 		}
 
-		if(len < sadb_ext->sadb_ext_len)
+		if(len < sadb_ext->sadb_ext_len) {
+			DEBUG_PRINT("Error: len < sadb_ext->sadb_ext_len\n");
+			sa_free(sa);
 			return false;
+		}
 		len -= sadb_ext->sadb_ext_len;
 		sadb_ext = (struct sadb_ext*)((uint8_t*)sadb_ext + sadb_ext->sadb_ext_len * 8);
 	}
 
-	return true;
+	return sapd_add_sa(sapd, sa);
 }
 
-static bool sadb_expire_process(struct sadb_msg* recv_msg) {
+static bool sadb_acquire_process(SAPD* sapd, struct sadb_msg* recv_msg) {
+	return false;
+}
+
+static bool sadb_expire_process(SAPD* sapd, struct sadb_msg* recv_msg) {
 	int len = recv_msg->sadb_msg_len - (sizeof(struct sadb_msg) / 8);
 	struct sadb_ext* sadb_ext = (struct sadb_ext*)((uint8_t*)recv_msg + sizeof(struct sadb_msg));
 	struct sadb_sa* sadb_sa = NULL;
@@ -218,12 +234,14 @@ static bool sadb_expire_process(struct sadb_msg* recv_msg) {
 				sadb_address_destination = (struct sadb_address*)sadb_ext;
 				break;
 			default:
-				DEBUG_PRINT("Error\n");
+				DEBUG_PRINT("Error: sadb_ext_type = %d\n", sadb_ext->sadb_ext_type);
 				break;
 		}
 
-		if(len < sadb_ext->sadb_ext_len)
+		if(len < sadb_ext->sadb_ext_len) {
+			DEBUG_PRINT("Error: len < sadb_ext->sadb_ext_len\n");
 			return false;
+		}
 		len -= sadb_ext->sadb_ext_len;
 		sadb_ext = (struct sadb_ext*)((uint8_t*)sadb_ext + sadb_ext->sadb_ext_len * 8);
 	}
@@ -233,17 +251,120 @@ static bool sadb_expire_process(struct sadb_msg* recv_msg) {
 	return true;
 }
 
-static bool sadb_flush_process(struct sadb_msg* recv_msg) {
-	DEBUG_PRINT("flush\n");
+static bool sadb_flush_process(SAPD* sapd, struct sadb_msg* recv_msg) {
+	//TODO remove all
 
 	return true;
 }
 
-static bool sadb_dump_process(struct sadb_msg* recv_msg) {
-	return sadb_get_process(recv_msg);
+static bool sadb_dump_process(SAPD* sapd, struct sadb_msg* recv_msg) {
+	return sadb_get_process(sapd, recv_msg);
 }
 
-bool sadb_set(int vmid) {
+static bool sadb_x_spdupdate_process(SAPD* sapd, struct sadb_msg* recv_msg) {
+	//TODO fix update
+	int len = recv_msg->sadb_msg_len - (sizeof(struct sadb_msg) / 8);
+	SP* sp = sp_alloc(len * 8);
+	sp->len = len;
+	memcpy(sp->data, (uint8_t*)recv_msg + sizeof(struct sadb_msg), len * 8);
+	struct sadb_ext* sadb_ext = (struct sadb_ext*)(sp->data);
+	while(len) {
+		switch(sadb_ext->sadb_ext_type) {
+			case SADB_EXT_RESERVED:
+				break;
+			case SADB_X_EXT_POLICY:
+				sp->policy = (struct sadb_x_policy*)sadb_ext;
+				break;
+			case SADB_EXT_LIFETIME_CURRENT:
+				sp->lifetime_current = (struct sadb_lifetime*)sadb_ext;
+				break;
+			case SADB_EXT_LIFETIME_HARD:
+				sp->lifetime_hard = (struct sadb_lifetime*)sadb_ext;
+				break;
+			case SADB_EXT_LIFETIME_SOFT:
+				sp->lifetime_soft = (struct sadb_lifetime*)sadb_ext;
+				break;
+			case SADB_EXT_ADDRESS_SRC:
+				sp->address_src = (struct sadb_address*)sadb_ext;
+				break;
+			case SADB_EXT_ADDRESS_DST:
+				sp->address_dst = (struct sadb_address*)sadb_ext;
+				break;
+			default:
+				DEBUG_PRINT("Error: sadb_ext_type = %d\n", sadb_ext->sadb_ext_type);
+				break;
+		}
+
+		if(len < sadb_ext->sadb_ext_len) {
+			DEBUG_PRINT("Error: len < sadb_ext->sadb_ext_len\n");
+			sp_free(sp);
+			return false;
+		}
+		len -= sadb_ext->sadb_ext_len;
+		sadb_ext = (struct sadb_ext*)((uint8_t*)sadb_ext + sadb_ext->sadb_ext_len * 8);
+	}
+	sp_free(sp);
+	return true;
+}
+
+static bool sadb_x_spdadd_process(SAPD* sapd, struct sadb_msg* recv_msg) {
+	return true;
+}
+
+static bool sadb_x_spddelete_process(SAPD* sapd, struct sadb_msg* recv_msg) {
+	return true;
+}
+
+static bool sadb_x_spddump_process(SAPD* sapd, struct sadb_msg* recv_msg) {
+	int len = recv_msg->sadb_msg_len - (sizeof(struct sadb_msg) / 8);
+	SP* sp = sp_alloc(len * 8);
+	sp->len = len;
+	memcpy(sp->data, (uint8_t*)recv_msg + sizeof(struct sadb_msg), len * 8);
+	struct sadb_ext* sadb_ext = (struct sadb_ext*)(sp->data);
+	while(len) {
+		switch(sadb_ext->sadb_ext_type) {
+			case SADB_EXT_RESERVED:
+				break;
+			case SADB_X_EXT_POLICY:
+				sp->policy = (struct sadb_x_policy*)sadb_ext;
+				break;
+			case SADB_EXT_LIFETIME_CURRENT:
+				sp->lifetime_current = (struct sadb_lifetime*)sadb_ext;
+				break;
+			case SADB_EXT_LIFETIME_HARD:
+				sp->lifetime_hard = (struct sadb_lifetime*)sadb_ext;
+				break;
+			case SADB_EXT_LIFETIME_SOFT:
+				sp->lifetime_soft = (struct sadb_lifetime*)sadb_ext;
+				break;
+			case SADB_EXT_ADDRESS_SRC:
+				sp->address_src = (struct sadb_address*)sadb_ext;
+				break;
+			case SADB_EXT_ADDRESS_DST:
+				sp->address_dst = (struct sadb_address*)sadb_ext;
+				break;
+			default:
+				DEBUG_PRINT("Error: sadb_ext_type = %d\n", sadb_ext->sadb_ext_type);
+				break;
+		}
+
+		if(len < sadb_ext->sadb_ext_len) {
+			DEBUG_PRINT("Error: len < sadb_ext->sadb_ext_len\n");
+			sp_free(sp);
+			return false;
+		}
+		len -= sadb_ext->sadb_ext_len;
+		sadb_ext = (struct sadb_ext*)((uint8_t*)sadb_ext + sadb_ext->sadb_ext_len * 8);
+	}
+	if(!sapd_add_sp(sapd, sp)) {
+		sp_free(sp);
+		return false;
+	}
+
+	return true;
+}
+
+static bool sadb_x_spdflush_process(SAPD* sapd, struct sadb_msg* recv_msg) {
 	return true;
 }
 
@@ -263,6 +384,23 @@ bool sadb_dump(int fd) {
 	memset(&msg, 0, sizeof(struct sadb_msg));
 	msg.sadb_msg_version = PF_KEY_V2;
 	msg.sadb_msg_type = SADB_DUMP;
+	msg.sadb_msg_satype = 0;
+	msg.sadb_msg_len = sizeof(msg) / 8;
+	msg.sadb_msg_pid = getpid();
+
+	int length = write(fd, &msg, sizeof(struct sadb_msg));  
+	if(length != sizeof(struct sadb_msg)) {
+		printf("Error\n");
+		return false;
+	}
+	return true;
+}
+
+bool sadb_x_spddump(int fd) {
+	struct sadb_msg msg;
+	memset(&msg, 0, sizeof(struct sadb_msg));
+	msg.sadb_msg_version = PF_KEY_V2;
+	msg.sadb_msg_type = SADB_X_SPDDUMP;
 	msg.sadb_msg_satype = 0;
 	msg.sadb_msg_len = sizeof(msg) / 8;
 	msg.sadb_msg_pid = getpid();
@@ -310,41 +448,99 @@ bool sadb_get(int fd, struct sadb_sa* sa, struct sadb_address* source, struct sa
 	return true;
 }
 
-int sadb_process(int fd) {
+int sadb_process(int fd, SAPD* sapd) {
 	uint8_t* buf[PFKEY_BUF_SIZE];
 	struct sadb_msg* recv_msg = (struct sadb_msg*)buf;
-	while(1) {
-		int length = read(fd, recv_msg, PFKEY_BUF_SIZE);
-		if(length < 0) {
-			printf("Error\n");
-			return -3;
-		}
-		switch(recv_msg->sadb_msg_type) {
-			case SADB_UPDATE:
-				sadb_update_process(recv_msg);
-				break;
-			case SADB_ADD:
-				sadb_add_process(recv_msg);
-				break;
-			case SADB_DELETE:
-				sadb_delete_process(recv_msg);
-				break;
-			case SADB_GET:
-				sadb_get_process(recv_msg);
-				break;
-			case SADB_EXPIRE:
-				sadb_expire_process(recv_msg);
-				break;
-			case SADB_FLUSH:
-				sadb_flush_process(recv_msg);
-				break;
-			case SADB_DUMP:
-				sadb_dump_process(recv_msg);
-				break;
-			default:
-				DEBUG_PRINT("Error %d\n", recv_msg->sadb_msg_type);
-				break;
-		}
+	int length = read(fd, recv_msg, PFKEY_BUF_SIZE);
+	if(length < 0) {
+		//printf("Error: %s\n");
+		DEBUG_PRINT("Error: sadb_process\n");
+		return -3;
+	}
+	switch(recv_msg->sadb_msg_type) {
+		case SADB_UPDATE:
+			//TODO not yet support update
+			DEBUG_PRINT("SADB UPDATE\n");
+			sadb_update_process(sapd, recv_msg);
+			break;
+		case SADB_ADD:
+			DEBUG_PRINT("SADB ADD\n");
+			sadb_add_process(sapd, recv_msg);
+			break;
+		case SADB_DELETE:
+			DEBUG_PRINT("SADB DELETE\n");
+			sadb_delete_process(sapd, recv_msg);
+			break;
+		case SADB_GET:
+			DEBUG_PRINT("SADB GET\n");
+			sadb_get_process(sapd, recv_msg);
+			break;
+		case SADB_ACQUIRE:
+			DEBUG_PRINT("SADB ACQUIRE\n");
+			sadb_acquire_process(sapd, recv_msg);
+			break;
+		case SADB_EXPIRE:
+			DEBUG_PRINT("SADB EXPIRE\n");
+			sadb_expire_process(sapd, recv_msg);
+			break;
+		case SADB_FLUSH:
+			DEBUG_PRINT("SADB FLUSH\n");
+			sadb_flush_process(sapd, recv_msg);
+			break;
+		case SADB_DUMP:
+			DEBUG_PRINT("SADB DUMP\n");
+			sadb_dump_process(sapd, recv_msg);
+			break;
+		case SADB_X_PROMISC:
+			DEBUG_PRINT("SADB_X PROMISC\n");
+			break;
+		case SADB_X_PCHANGE:
+			DEBUG_PRINT("SADB_X PCHANGE\n");
+			break;
+		case SADB_X_SPDUPDATE:
+			DEBUG_PRINT("SADB_X SPD UPDATE\n");
+			sadb_x_spdupdate_process(sapd, recv_msg);
+			break;
+		case SADB_X_SPDADD:
+			DEBUG_PRINT("SADB_X SPD ADD\n");
+			sadb_x_spdadd_process(sapd, recv_msg);
+			break;
+		case SADB_X_SPDDELETE:
+			DEBUG_PRINT("SADB_X SPD DELETE\n");
+			sadb_x_spddelete_process(sapd, recv_msg);
+			break;
+		case SADB_X_SPDGET:
+			DEBUG_PRINT("SADB_X SPD GET\n");
+			break;
+		case SADB_X_SPDACQUIRE:
+			DEBUG_PRINT("SADB_X SPD ACQUIRE\n");
+			break;
+		case SADB_X_SPDDUMP:
+			DEBUG_PRINT("SADB_X SPD DUMP\n");
+			sadb_x_spddump_process(sapd, recv_msg);
+			break;
+		case SADB_X_SPDFLUSH:
+			DEBUG_PRINT("SADB_X SPD FLUSH\n");
+			sadb_x_spdflush_process(sapd, recv_msg);
+			break;
+		case SADB_X_SPDSETIDX:
+			DEBUG_PRINT("SADB_X SPD SET IDX\n");
+			break;
+		case SADB_X_SPDEXPIRE:
+			DEBUG_PRINT("SADB_X SPD EXPIRE\n");
+			break;
+		case SADB_X_SPDDELETE2:
+			DEBUG_PRINT("SADB_X SPD DELETE2\n");
+			break;
+		case SADB_X_NAT_T_NEW_MAPPING:
+			DEBUG_PRINT("SADB_X NAT T NEW MAPPING\n");
+			break;
+		case SADB_X_MIGRATE:
+			DEBUG_PRINT("SADB_X MIGRATE\n");
+			break;
+		default:
+			DEBUG_PRINT("Error %d\n", recv_msg->sadb_msg_type);
+			break;
 	}
 
 	return 0;
